25d391391213b46903f799755e54fb96e85e82c6
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2023-08-16 19:14:30 +0530

    Add food icons

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Box, Typography,AppBar ,Toolbar } from '@mui/material'
+import FastfoodIcon from '@mui/icons-material/Fastfood';
 const Header = () => {
   return (
     <>
        <Box>
         <AppBar component={'nav'} sx={{bgcolor:"black"}}>
           <Toolbar>
-          <Typography> My Restorent App</Typography>
+          <Typography color={'goldenrod'} variant="h6" component={'div'}>  <FastfoodIcon/> My Restorent App
+          </Typography>
               
-          <Typography> My Restorent App</Typography>
+          
           </Toolbar>
         </AppBar>
         </Box> 
