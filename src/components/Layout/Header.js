a71829cd9fd43929565774efa93eed9bca0fe58f
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2023-08-16 21:52:07 +0530

    Update css

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,16 +1,34 @@
 import React from 'react'
 import { Box, Typography,AppBar ,Toolbar } from '@mui/material'
 import FastfoodIcon from '@mui/icons-material/Fastfood';
+import {Link} from 'react-router-dom';
+import "../../styles/HeaderStyle.css";
 const Header = () => {
   return (
     <>
        <Box>
         <AppBar component={'nav'} sx={{bgcolor:"black"}}>
           <Toolbar>
-          <Typography color={'goldenrod'} variant="h6" component={'div'}>  <FastfoodIcon/> My Restorent App
+          <Typography color={'goldenrod'} variant="h6" component={'div'} sx={{flexGrow:1}}>  <FastfoodIcon/> My Restorent App
           </Typography>
-              
-          
+              <Box sx={{display:{xs:'none',sm:"block"}}}>
+                 <ul className='navigation-menu'>
+                  <li>
+                  <Link  to={'/ home'}>Home</Link>
+                    </li>
+                    <li>
+                  <Link  to={'/menu'}>Menu</Link>
+                    </li>
+                    <li>
+                  <Link  to={'/ about'}>About</Link>
+                    </li>
+                    <li>
+                  <Link  to={'/ contact'}>Contact</Link>
+                    </li>
+                    
+                  </ul>
+
+              </Box>
           </Toolbar>
         </AppBar>
         </Box> 
