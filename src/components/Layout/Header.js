31e029c5f3f696198163c8285fcf95aa8e849fa7
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2023-08-19 23:57:44 +0530

    Update Header.js

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,44 +1,91 @@
-import React from 'react'
-import { Box, Typography,AppBar ,Toolbar,IconButton } from '@mui/material'
+import React, { useState } from 'react';
+import { Box, Typography, AppBar, Toolbar, IconButton, Drawer,Divider } from '@mui/material';
 import FastfoodIcon from '@mui/icons-material/Fastfood';
-
 import MenuIcon from '@mui/icons-material/Menu';
-import {Link} from 'react-router-dom';
-import "../../styles/HeaderStyle.css";
+import { Link } from 'react-router-dom';
+import '../../styles/HeaderStyle.css';
+
 const Header = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  // Handle menu click
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  // Menu drawer content
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography color="goldenrod" variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <FastfoodIcon /> My Restaurant App
+      </Typography>
+      <Divider/>
+      <ul  className="mobile-navigation">
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/menu">Menu</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </Box>
+  );
+
   return (
     <>
-       <Box>
-        <AppBar component={'nav'} sx={{bgcolor:"black"}}>
+      <Box>
+        <AppBar component="nav" sx={{ bgcolor: 'black' }}>
           <Toolbar>
-            <IconButton color="inherit" arial-label="open drawer" edge="start" sx={{mr:2,display:{sm:"none"},}}>
-              <MenuIcon/>
+            <IconButton color="inherit"
+             aria-label="open drawer"
+              edge="start" 
+              sx={{ mr: 2, display: { sm: 'none' },}} 
+            onClick={handleDrawerToggle}>
+            
+              <MenuIcon />
             </IconButton>
-          <Typography color={'goldenrod'} variant="h6" component={'div'} sx={{flexGrow:1}}>  <FastfoodIcon/> My Restorent App
-          </Typography>
-              <Box sx={{display:{xs:'none',sm:"block"}}}>
-                 <ul className='navigation-menu'>
-                  <li>
-                  <Link  to={'/ home'}>Home</Link>
-                    </li>
-                    <li>
-                  <Link  to={'/menu'}>Menu</Link>
-                    </li>
-                    <li>
-                  <Link  to={'/ about'}>About</Link>
-                    </li>
-                    <li>
-                  <Link  to={'/ contact'}>Contact</Link>
-                    </li>
-                    
-                  </ul>
-
-              </Box>
+            <Typography color="goldenrod" variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <FastfoodIcon /> My Restaurant App
+            </Typography>
+            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+              <ul className="navigation-menu">
+                <li>
+                  <Link to="/home">Home</Link>
+                </li>
+                <li>
+                  <Link to="/menu">Menu</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/contact">Contact</Link>
+                </li>
+              </ul>
+            </Box>
           </Toolbar>
         </AppBar>
-        </Box> 
+        <Box component="nav">
+          <Drawer variant="temporary"
+           open={mobileOpen}
+            onClose={handleDrawerToggle} 
+            sx={{ display: { xs: 'block', sm: 'none' } ,
+            "&.MuiDrawer-paper":{
+              boxSizing:"border-box", 
+              width:"240px",
+            }}}>
+            {drawer}
+          </Drawer>
+        </Box>
+      </Box>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
