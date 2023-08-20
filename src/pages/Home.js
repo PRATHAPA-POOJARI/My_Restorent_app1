c04ef65e890b9ccc07c76e451ff842861cc2391b
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2023-08-20 18:40:02 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,23 @@
-import React from 'react'
-import Layout from '../components/Layout/Layout'
+import React from 'react';
+import Layout from '../components/Layout/Layout'; // Corrected import path
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <Layout>
-        <h1> Home page</h1>
-
+      <div className='home'>
+        <div className='headerContainer'>
+          <h1>Food website</h1>
+          <p>Best Food in India</p>
+          <Link to="/menu">
+            <button>
+              ORDER NOW
+            </button>
+          </Link>
+        </div>
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
