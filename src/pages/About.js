2dab4d07131f15a3c9dc30cc6563a6c5349c0494
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2023-08-23 23:34:09 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
+import {Box,Typography} from '@mui/material';
 
 const About = () => {
   return (
     <Layout>
-        <h1> About</h1>
+       <Box>
+         <Typography>
+          welcome to my restorent
+          </Typography>
+          <p> lorem</p>
+        </Box>
 
     </Layout>
   )
