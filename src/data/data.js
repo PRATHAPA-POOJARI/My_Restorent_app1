9b6f6cad523f7b9581d6b80947e6072ea090899b
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2023-08-27 23:11:35 +0530

    menu page created and images are inserted

---
+++ src/data/data.js
@@ -0,0 +1,51 @@
+import Dosa from '../images/dosa.jpg';
+import Chola from '../images/chhola.jpg';
+import Idli from '../images/idli.jpg';
+import MasalaDosa from '../images/masala.jpg';
+import Panner from '../images/paneer.jpg';
+import Gujrati from '../images/gujrati.jpeg';
+
+export const  MenuList =[
+    {
+        name:'Dosa',
+        description: 'A very tasty dosa can be prepared using Field marrow/Mangalore cucumber/Malabar cucumber. They are an age-old breakfast too. A well fermented rice batter makes us soft, fluffy, to-die-for dosas. These field marrow dosas have a nice, distinctive taste and smell of field marrow. These dosas are served with lots of homemade butter & a spicy coconut chutney. 😍'
+        image:Dosa,
+        price:200
+   
+    }
+    {
+        name:'Dosa',
+        description: 'A very tasty dosa can be prepared using Field marrow/Mangalore cucumber/Malabar cucumber. They are an age-old breakfast too. A well fermented rice batter makes us soft, fluffy, to-die-for dosas. These field marrow dosas have a nice, distinctive taste and smell of field marrow. These dosas are served with lots of homemade butter & a spicy coconut chutney. 😍'
+        image:Dosa,
+        price:200
+   
+    }
+    {
+        name:'Dosa',
+        description: 'A very tasty dosa can be prepared using Field marrow/Mangalore cucumber/Malabar cucumber. They are an age-old breakfast too. A well fermented rice batter makes us soft, fluffy, to-die-for dosas. These field marrow dosas have a nice, distinctive taste and smell of field marrow. These dosas are served with lots of homemade butter & a spicy coconut chutney. 😍'
+        image:Dosa,
+        price:200
+   
+    }
+    {
+        name:'Dosa',
+        description: 'A very tasty dosa can be prepared using Field marrow/Mangalore cucumber/Malabar cucumber. They are an age-old breakfast too. A well fermented rice batter makes us soft, fluffy, to-die-for dosas. These field marrow dosas have a nice, distinctive taste and smell of field marrow. These dosas are served with lots of homemade butter & a spicy coconut chutney. 😍'
+        image:Dosa,
+        price:200
+   
+    }
+    {
+        name:'Dosa',
+        description: 'A very tasty dosa can be prepared using Field marrow/Mangalore cucumber/Malabar cucumber. They are an age-old breakfast too. A well fermented rice batter makes us soft, fluffy, to-die-for dosas. These field marrow dosas have a nice, distinctive taste and smell of field marrow. These dosas are served with lots of homemade butter & a spicy coconut chutney. 😍'
+        image:Dosa,
+        price:200
+   
+    }
+    {
+        name:'Dosa',
+        description: 'A very tasty dosa can be prepared using Field marrow/Mangalore cucumber/Malabar cucumber. They are an age-old breakfast too. A well fermented rice batter makes us soft, fluffy, to-die-for dosas. These field marrow dosas have a nice, distinctive taste and smell of field marrow. These dosas are served with lots of homemade butter & a spicy coconut chutney. 😍'
+        image:Dosa,
+        price:200
+   
+    }
+]
